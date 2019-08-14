c19e89a90134fd19abc706412905a03894c63fd7
Emma Zarate <2327788+yoshimii@users.noreply.github.com>
2019-08-14 15:49:26 -0500

    built photo and screen components

--- src/APODCard.js
+++ src/APODCard.js
@@ -2,11 +2,11 @@ import React from "react";
 
 const APODCard = props => {
     return (
-        <div className="photo">
+        <div className="photo" key={props.id}>
             <img src={props.url}></img>
             <p>{props.description}</p>
         </div>
-    )
-}
+    );
+};
 
 export default APODCard;
--- src/Planetarium.js
+++ src/Planetarium.js
@@ -9,6 +9,18 @@ export default function App () {
 
     }, []);
     return (
-        
-    )
-}
+        <>
+        <div className="screen">
+            {photos.map(photo => {
+                return (
+                    <APODCard
+                    key={photo.id}
+                    image={photo.url}
+                    description={photo.description}
+                )
+            })}
+            <APODCard/>
+        </div>
+        </>
+    );
+};
