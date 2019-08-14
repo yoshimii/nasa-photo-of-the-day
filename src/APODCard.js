1af7de7e8ef16ce28e1470823bf754de44040828
Emma Zarate <2327788+yoshimii@users.noreply.github.com>
2019-08-14 15:43:57 -0500

    imported and exported necessary data and set up components and props

--- src/APODCard.js
+++ src/APODCard.js
@@ -1,2 +1,12 @@
 import React from "react";
-import Planetarium from "./Planetarium";
+
+const APODCard = props => {
+    return (
+        <div className="photo">
+            <img src={props.url}></img>
+            <p>{props.description}</p>
+        </div>
+    )
+}
+
+export default APODCard;
---
+++ src/Planetarium.js
@@ -0,0 +1,14 @@
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+import APODCard from "./APODCard";
+
+export default function App () {
+    const [photos, setPhotos] = useState([]);
+
+    useEffect(()=> {
+
+    }, []);
+    return (
+        
+    )
+}
